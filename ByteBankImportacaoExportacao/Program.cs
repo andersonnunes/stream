37ad294854fd928477a113e093ca897c74781a49
Anderson <anderson.nunes1988@gmail.com>
2019-01-17 10:35:14 -0200

    Fazendo parse e StreamWriter

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -7,18 +7,8 @@ namespace ByteBankImportacaoExportacao
     {
         static void Main(string[] args)
         {
-            var enderecoDoArquivo = "contas.txt";
-
-            using(var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            using(var leitor = new StreamReader(fluxoDoArquivo))
-            {
-
-                while(!leitor.EndOfStream)
-                {
-                    var linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
-                }
-            }
+            CriarArquivoComWriter();
+            Console.WriteLine("Aplicação finalizada ...");
 
             Console.ReadKey();
         }
